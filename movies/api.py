65c86a28de3229ec0d40ee30005d967ab53e25eb
Klaus Laube <kplaube@gmail.com>
2017-01-04 20:33:13 -0200

    Post and authentication solved

--- movies/api.py
+++ movies/api.py
@@ -13,3 +13,12 @@ class MovieResource(DjangoResource):
 
     def list(self):
         return Movie.objects.all()
+
+    def create(self):
+        return Movie.objects.create(
+            title=self.data['title'],
+            description=self.data['description'],
+        )
+
+    def is_authenticated(self):
+        return True
