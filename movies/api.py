9b35fbd71095b2fe4077ea1b68c408c2c97215cf
Klaus Laube <kplaube@gmail.com>
2017-01-05 11:06:16 -0200

    Add the details endpoint

--- movies/api.py
+++ movies/api.py
@@ -1,6 +1,7 @@
 from restless.dj import DjangoResource
 from restless.preparers import FieldsPreparer
 
+from django.shortcuts import get_object_or_404
 from movies.models import Movie
 
 
@@ -19,6 +20,9 @@ class MovieResource(DjangoResource):
             title=self.data['title'],
             description=self.data['description'],
         )
+
+    def detail(self, pk):
+        return get_object_or_404(Movie, pk=pk)
 
     def is_authenticated(self):
         return True
